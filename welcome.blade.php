4bffe87464de0b4d7da13f9cb10e8545acebdf86
Charlene393 <131701102+Charlene393@users.noreply.github.com>
2024-05-30 11:36:35 +0300

    Add files via upload
    Contains homepage files

---
+++ welcome.blade.php
@@ -0,0 +1,280 @@
+@include('layout.header')
+  <section id="hero">
+    <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Welcome to RecycleConnect</h1>
+      <h2>Turn Plastic Waste into Opportunity: Connect, Recycle, and Transform!</h2>
+      <a href="#about" class="btn-get-started">Get Started</a>
+    </div>
+  </section>
+
+  <main id="main">
+
+
+    <section id="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row about-container">
+
+          <div class="col-lg-6 content order-lg-1 order-2">
+            <h2 class="title">ABOUT US</h2>
+            <p>
+            Revolutionize plastic waste management in Kenya by leveraging technology to create an effective and convenient platform for connecting waste producers with recycling organizations and promoting sustainable practices.
+            </p>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bi bi-people"></i></div>
+              <h4 class="title"><a href="/feedback">Social impact</a></h4>
+              <p class="description">RecycleConnect empowers communities to recycle, reduce waste, and thrive. It fosters collaboration through material exchange, creates green jobs, and educates for a sustainable future.<a href="/signin">Join us and make a difference!</a> </p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div class="icon"><i class="bi bi-card-checklist"></i></div>
+              <h4 class="title"><a href="/location">Smart technology</a></h4>
+              <p class="description">RecycleConnect leverages technology to connect plastic waste generators with recyclying organizations.  Their map-based platform simplifies waste disposal and tracks progress, empowering a greener future. </p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <div class="icon"><i class="bi bi-binoculars"></i></div>
+              <h4 class="title"><a href="/guide">Guide</a></h4>
+              <p class="description">RecycleConnect empowers you! Search their platform to identify your plastic waste and learn the best disposal methods. It's easy recycling,<a href= "/guide">one click at a time.</a></p>
+            </div>
+
+
+          </div>
+
+          <div class="col-lg-6 background order-lg-2 order-1" data-aos="fade-left" data-aos-delay="100"></div>
+        </div>
+        <section id="facts">
+            <div class="container" data-aos="fade-up">
+              <div class="section-header">
+                <h3 class="section-title">Statistics</h3>
+                <p class="section-description"></p>
+              </div>
+              <div class="row counters">
+
+                <div class="col-lg-3 col-6 text-center">
+
+                    <i class="bi bi-person-add" style="font-size: 4em;"></i>
+                    <span data-purecounter-start="0" data-purecounter-end="10" data-purecounter-duration="1" class="purecounter"></span>
+                  <p><h5>Plastic waste generators</h5></p>
+                </div>
+
+                <div class="col-lg-3 col-6 text-center">
+                    <i class="bi bi-recycle" style="font-size: 4em;"></i>
+                    <span data-purecounter-start="0" data-purecounter-end="534" data-purecounter-duration="1" class="purecounter"></span>
+                  <p><h5>Recycling organizations</h5></p>
+                </div>
+
+                <div class="col-lg-3 col-6 text-center">
+                    <i class="bi bi-bar-chart-line" style="font-size: 4em;"></i><span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter"></span>
+                  <p><h5>Plastic waste recycled</h5></p>
+                </div>
+
+              </div>
+              <a href="/about" class="btn-read-more">Read More</a>
+              </div>
+
+            </div>
+          </section>
+      </div>
+    </section><!-- End About Section -->
+    <section id="events">
+        <div class="container" data-aos="fade-up">
+          <div class="section-header">
+            <h3 class="section-title">Events and News</h3>
+            <p class="section-description">Get the latest events and news about plastic over here.
+                <i class="fas fa-search search-icon"></i>
+            </p>
+          </div>
+          <div class="row">
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-newspaper"></i></a></div>
+              <h4 class="title"><a href="">News</a></h4>
+              <p class="description"> Countries and regions are increasingly implementing bans on single-use plastics, such as Canada's recent ban.
+                <a href ="https://www.independent.co.uk/news/ap-ottawa-united-nations-environment-programme-exxonmobil-canada-b2533007.html">Click here to learn more</a>
+              </p>
+            </div>
+
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-newspaper"></i></a></div>
+              <h4 class="title"><a href="">NEWS</a></h4>
+              <p class="description">Recent studies have found microplastics (tiny plastic particles) in surprising places, including human blood for the first time.
+                <a href="https://www.theguardian.com/environment/2022/mar/24/microplastics-found-in-human-blood-for-first-time"Click here to learn more</a>
+              </p>
+            </div>
+
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-people-fill"></i></a></div>
+              <h4 class="title"><a href="">Events</a></h4>
+              <p class="description">Plastic collection in Nairobi river.
+                <a href="">Click here to learn more</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <!-- ======= Services Section ======= -->
+    <section id="services">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title">Services</h3>
+          <p class="section-description">Services offered by RecycleConnect</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                <div class="box">
+                  <div class="icon"><a href=""><i class="bi bi-people-fill"></i></a></div>
+                  <h4 class="title"><a href="/signin">Registration and profiles</a></h4>
+                  <p class="description">Users can easily sign up to a platform and sign in to gain full access to our services</p>
+                </div>
+              </div>
+          <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-briefcase"></i></a></div>
+              <h4 class="title"><a href="">Creating a source of income</a></h4>
+              <p class="description">RecycleConnect enables people to make money by selling the plastic to recycling organizations.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-calendar4-week"></i></a></div>
+              <h4 class="title"><a href="">Schedeling pick up</a></h4>
+              <p class="description">Plastic waste generators can schedule pickups</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-bar-chart"></i></a></div>
+              <h4 class="title"><a href="">Progress report</a></h4>
+              <p class="description">Members of the commnity can check their progress reports on how much they have contributed in making Kenya plastic clean.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+            <div class="box">
+              <div class="icon"><a href=""><i class="bi bi-binoculars"></i></a></div>
+              <h4 class="title"><a href="">Plastic disposal Guide</a></h4>
+              <p class="description">Availability of a disposal guide that helps people to learn on how they can dispose plastic.</p>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Call To Action Section ======= -->
+    <section id="call-to-action">
+      <div class="container">
+        <div class="row" data-aos="zoom-in">
+          <div class="col-lg-9 text-center text-lg-start">
+            <h3 class="cta-title">SUPPORT AND DONATE</h3>
+            <p class="cta-text"> Join us in making Kenya a plastic clean country.</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="/support">Support</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- ======= Portfolio Section ======= -->
+    <
+    <!-- ======= Team Section ======= -->
+   <!-- End Team Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact">
+      <div class="container">
+        <div class="section-header">
+          <h3 class="section-title">Contact</h3>
+          <p class="section-description">Contact RecycleConnect</p>
+        </div>
+      </div>
+
+
+            <div class="container mt-5">
+        <div class="row justify-content-center">
+
+          <div class="col-lg-3 col-md-4">
+
+            <div class="info">
+              <div>
+                <i class="bi bi-geo-alt"></i>
+                <p>Nairobi</p>
+              </div>
+
+              <div>
+                <i class="bi bi-envelope"></i>
+                <p>charlenenjambi@gmail.com</p>
+              </div>
+
+              <div>
+                <i class="bi bi-phone"></i>
+                <p>+254113268139</p>
+              </div>
+            </div>
+
+            <div class="social-links">
+                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <div class="form">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="form-group mt-3">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+                <div class="form-group mt-3">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                </div>
+                <div class="form-group mt-3">
+                  <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+@include('layout.footer')
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
