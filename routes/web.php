17747939ee882e0f0cbe8e5ed16f0fb7e5e83231
Charlene447 <Charlene.Mbugua@strathmore.edu>
2024-06-12 15:38:51 +0300

    Save local changes before pulling

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
+Route::get('/', [DashboardController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/event', function () {
+Route::controller(PaymentController::class)
+    ->prefix('payment')
+    ->as('payment.')
+    ->group(function() {
+        Route::get('/initiatePush','intiateStkPush')->name('initiatePush');
+    });
+
+// Route::get('/event', [EventController::class, 'index']);
+Route::get('/event', function() {
     return view('eventandnews');
 });
